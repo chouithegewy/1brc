af8388538830bcdd6ffccf5ca806b5c9cc45be50
David Childs <combatburrito@gmail.com>
2025-10-29 18:37:55 -0700

    todo: chunk_parser

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,46 @@ use std::os::fd::AsRawFd;
 use std::thread;
 
 fn bytes_to_float(bytes: [u8; 5]) -> Option<f32> {
+    //naive first
     let sign = if bytes[0] == 45 {
         // minus sign (-)
-        -1
-    } else {
         1
+    } else {
+        0
+    };
+    let mut integer_part = -100;
+    let mut fractional_part = -0.1f32;
+    let mut point: usize = 0;
+    for i in sign..bytes.len() {
+        if bytes[i] == b'.' {
+            point = i;
+        }
+    }
+    fractional_part = match point {
+        1 => 1f32 / bytes[2] as f32,
+        2 => 1f32 / bytes[3] as f32,
+        3 => 1f32 / bytes[4] as f32,
+        _ => panic!("Failed to parse float -- invalid index"),
+    };
+    integer_part = match point {
+        1 => bytes[0] as i32,
+        2 => bytes[0] as i32 * 10 + bytes[1] as i32,
+        3 => bytes[1] as i32 * 10 + bytes[2] as i32,
+        _ => panic!("Failed to parse float -- invalid index"),
     };
 
-    let offset: usize = if sign > 0 { 0 } else { 1 };
-
-    let integer_part = (bytes[offset + 1] as u16) | ((bytes[0] as u16) << 8);
-
-    if integer_part > 99 {
-        return None;
-    }
-
-    let fractional_part = bytes[2] as f32 / 10.0;
     Some(integer_part as f32 + fractional_part)
+
+    //let offset: usize = if sign > 0 { 0 } else { 1 };
+
+    //let integer_part = (bytes[offset + 1] as u16) | ((bytes[0] as u16) << 8);
+
+    //if integer_part > 99 {
+    //    return None;
+    //}
+
+    //let fractional_part = bytes[2] as f32 / 10.0;
+    //Some(integer_part as f32 + fractional_part)
 }
 
 fn find_next_newline(data: &[u8]) -> Vec<usize> {
@@ -32,6 +55,16 @@ fn find_next_newline(data: &[u8]) -> Vec<usize> {
         .take(1)
         .collect()
 }
+
+fn find_next_semicolon(data: &[u8]) -> Vec<usize> {
+    data.iter()
+        .enumerate()
+        .filter(|&(_, byte)| *byte == b';')
+        .map(|(i, _)| i)
+        .take(1)
+        .collect()
+}
+
 
 fn get_thread_count() -> usize {
     thread::available_parallelism()
@@ -55,6 +88,9 @@ fn chunker(size: usize, data: &[u8]) -> Vec<(usize, usize)> {
     }
 
     chunks
+}
+
+fn chunk_processor(data: &[u8]) {
 }
 
 fn main() -> io::Result<()> {
@@ -86,9 +122,7 @@ fn main() -> io::Result<()> {
 
     let mut handles = vec![];
     for chunk in chunks {
-        let j_handle = thread::spawn(move ||
-            println!("process ze data: chunk {:?}", chunk)
-        );
+        let j_handle = thread::spawn(move || println!("process ze data: chunk {:?}", chunk));
         handles.push(j_handle);
     }
 
